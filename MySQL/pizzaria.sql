2c0d6896cb31fed5d154d5bfff5813348bba0bd0
Lucas Buzo <lucabtei@outlook.com>
2021-04-22 23:21:08 -0300

    pizza

--- MySQL/pizzaria.sql
+++ MySQL/pizzaria.sql
@@ -23,12 +23,16 @@ create table tb_pizza(
 );
 
 	insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Portuguesa", 1, 24.99, true,);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Marguerita", 2, 29.99, false);
-    
-select * from tb_categoria;
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Portuguesa", 1, 24.99, true);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Queijo", 4, 34.99, false);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Chocolate", 5, 44.99, false);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("4 queijos", 1, 29.99, true);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Romeu e Julieta", 5, 21.99, true);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Brotinho queijo", 3, 14.99, false);
+    insert tb_pizza(sabor, id_categoria, preco, promocional) values ("Frango e Catupiry", 4, 29.99, false);
+
+select * from tb_pizza where preco > 30;
+select * from tb_pizza where preco between 20 and 40;
+select * from tb_pizza where sabor like "C%";
+select * from tb_pizza inner join tb_categoria on tb_categoria.id = tb_pizza.id_categoria;
+select * from tb_pizza where id_categoria = 4;
