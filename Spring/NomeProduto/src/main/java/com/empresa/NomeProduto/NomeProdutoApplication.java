aeb4286185cdeaaddb877c0b1e98f2ae36a3a27a
Lucas Buzo <lucabtei@outlook.com>
2021-04-27 17:50:56 -0300

    blog-initial

---
+++ Spring/NomeProduto/src/main/java/com/empresa/NomeProduto/NomeProdutoApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.NomeProduto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomeProdutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomeProdutoApplication.class, args);
+	}
+
+}
---
+++ Spring/NomeProduto/src/test/java/com/empresa/NomeProduto/NomeProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.NomeProduto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomeProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
